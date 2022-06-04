2be59bd1aeeaa24491fd0b95d43483a50e4c1484
yukihooked <78239937+yukihooked@users.noreply.github.com>
2022-06-04 09:35:22 -0400

    Create tnf_dumper.lua

---
+++ tools/tnf_dumper.lua
@@ -0,0 +1,11 @@
+local j_anticheat = game.ReplicatedStorage:WaitForChild("Game_Replicated", 9999999):WaitForChild("Game_Scripts", 999999):WaitForChild("ClientLoader"):WaitForChild("Loader")
+
+local func = require(j_anticheat)
+local old_func
+local counter = 0
+old_func = hookfunction(func, function(...)
+    counter += 1
+    local args = {...}
+    writefile(tostring(counter)..".lua", args[1])
+    return old_func(...)
+end)

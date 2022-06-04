58969cd7b2d454888af651f4916a6dfdd86762ae
yukihooked <78239937+yukihooked@users.noreply.github.com>
2022-06-04 09:35:53 -0400

    Update tnf_dumper.lua

--- tools/tnf_dumper.lua
+++ tools/tnf_dumper.lua
@@ -1,3 +1,4 @@
+-- Stick this in autoexec, this is arguably the worst method you could use
 local j_anticheat = game.ReplicatedStorage:WaitForChild("Game_Replicated", 9999999):WaitForChild("Game_Scripts", 999999):WaitForChild("ClientLoader"):WaitForChild("Loader")
 
 local func = require(j_anticheat)
